8015c84ffe3a2f516f1be550203dc9f756a0f3ba
stanislavmedved <145404670+stanislavmedved@users.noreply.github.com>
2024-05-21 12:40:53 +0300

    daaamn this is slow

--- lab7-sem4/lab7-sem4.cpp
+++ lab7-sem4/lab7-sem4.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <list>
+#include <chrono>
 
 /*
 std::vector
@@ -48,13 +49,56 @@ int main()
     // Создать вектор и список с 1 миллионом элементов
     std::vector<int> v(1000000);
     std::list<int> l(1000000);
+    {
+        // Вычислить объем памяти, используемой каждым контейнером
 
-    // Вычислить объем памяти, используемой каждым контейнером
+        // Для std::vector: размер массива * размер элемента
+        std::cout << "Объем памяти std::vector: " << v.size() * sizeof(v[0]) << " байт" << std::endl;
 
-    // Для std::vector: размер массива * размер элемента
-    std::cout << "Объем памяти std::vector: " << v.size() * sizeof(v[0]) << " байт" << std::endl;
+        // Для std::list: размер списка * (размер элемента + размер узла)
+        std::cout << "Объем памяти std::list: " << listSizeInBytes(l) << " байт" << std::endl;
+    }
+    {
+        // заполняем
+        for (int i = 0; i < 100000; ++i) {
+            v.insert(v.begin(), i);
+            l.push_front(i);
+        }
 
-    // Для std::list: размер списка * (размер элемента + размер узла)
-    std::cout << "Объем памяти std::list: " << listSizeInBytes(l) << " байт" << std::endl;
+        // таймер
+        auto start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < 100000; ++i) {
+            v[i] = i;
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        auto time_taken_vector_write = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        std::cout << "Время на запись std::vector " << time_taken_vector_write << " наносекунд" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < 100000; ++i) {
+            v[i];
+        }
+        end = std::chrono::high_resolution_clock::now();
+        auto time_taken_vector_read = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        std::cout << "Время на чтение std::vector " << time_taken_vector_read << " наносекунд" << std::endl;
+
+        int j = 0;
+        start = std::chrono::high_resolution_clock::now();
+        for (int x: l) {
+            x = j;
+            j++;
+        }
+        end = std::chrono::high_resolution_clock::now();
+        auto time_taken_list_write = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        std::cout << "Время на запись std::list " << time_taken_list_write << " наносекунд" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
+        for (int x : l) {
+            x;
+        }
+        end = std::chrono::high_resolution_clock::now();
+        auto time_taken_list_read = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        std::cout << "Время на чтение std::list " << time_taken_list_read << " наносекунд" << std::endl;
+    }
     return 0;
 }
